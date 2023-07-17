d3d3aa103ba65458b0611b7f9a09976c021bd01d
Víctor Carmona <vicarali@gmail.com>
2023-07-17 12:36:23 +0200

    Adjustments to 1st task solution

--- 01-task/src/components/ExpenseItem.js
+++ 01-task/src/components/ExpenseItem.js
@@ -3,13 +3,13 @@ import "./ExpenseItem.css";
 
 function ExpenseItem({ title, amount, date }) {
   return (
-    <li className="expense-item">
+    <div className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </li>
+    </div>
   );
 }
 
