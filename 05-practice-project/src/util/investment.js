cd4e04df28682f38dafe123bcdc5641971b9854b
VictorC <vicarali@gmail.com>
2024-03-10 16:49:44 +0100

    Modify investment calculator to produce all required values

--- 05-practice-project/src/util/investment.js
+++ 05-practice-project/src/util/investment.js
@@ -12,17 +12,22 @@ export function calculateInvestmentResults({
 }) {
   const annualData = [];
   let investmentValue = initialInvestment;
+  let totalInterest = 0,
+		totalInvestment = initialInvestment;
 
-  for (let i = 0; i < duration; i++) {
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
-      year: i + 1, // year identifier
-      interest: interestEarnedInYear, // the amount of interest earned in this year
-      valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
-    });
-  }
+	for (let i = 0; i < duration; i++) {
+		const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+		investmentValue += interestEarnedInYear + annualInvestment;
+		totalInterest += interestEarnedInYear;
+		totalInvestment += annualInvestment;
+		annualData.push({
+			year: i + 1, // year identifier
+			valueEndOfYear: investmentValue, // investment value at end of year
+			interest: interestEarnedInYear, // the amount of interest earned in this year
+			totalInterest: totalInterest,
+			totalInvestment: totalInvestment // investment added in this year
+		});
+	}
 
   return annualData;
 }
