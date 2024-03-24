426151e002824d23ed81e6670d9f29e1194aedd7
VictorC <vicarali@gmail.com>
2024-03-24 16:07:55 +0100

    09-practice-project: Remove Tailwind CSS from the project

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -1,9 +1,10 @@
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+		<>
+			<aside className="projects-listing"></aside>
+			<h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+		</>
+	);
 }
 
 export default App;
