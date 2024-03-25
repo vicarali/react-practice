845a1268449bb08f981c2a4507aae581f754e590
VictorC <vicarali@gmail.com>
2024-03-25 15:24:18 +0100

    Set up initial layout and styles for sidebar component

---
+++ 09-practice-project/src/components/ProjectsListing/ProjectsListing.jsx
@@ -0,0 +1,10 @@
+import "./ProjectsListing.css";
+
+export default function ProjectsListing() {
+	return (
+		<ul className="projects-listing">
+			<li className="projects-listing__item">Test 1</li>
+			<li className="projects-listing__item">Test 2</li>
+		</ul>
+	);
+}
--- 09-practice-project/src/components/Sidebar/Sidebar.jsx
+++ 09-practice-project/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,12 @@
+import ProjectsListing from "../ProjectsListing/ProjectsListing";
 import "./Sidebar.css";
 
 export default function Sidebar() {
-	return <aside className="sidebar"></aside>;
+	return (
+		<aside className="sidebar">
+			<h2 className="sidebar__title">YOUR PROJECTS</h2>
+			<button className="sidebar__button">+ Add Project</button>
+			<ProjectsListing />
+		</aside>
+	);
 }
