31070fe0195d95928865653d2f76ad39c52e0f6a
VictorC <vicarali@gmail.com>
2024-04-20 16:21:17 +0200

    Set up question overview component

---
+++ 12-practice-project/src/components/QuestionOverview/QuestionOverview.jsx
@@ -0,0 +1,9 @@
+import "./QuestionOverview.css";
+
+export function QuestionOverview() {
+	return (
+		<div className="question-overview">
+			<h2 className="question-overview__title">Test of a title</h2>
+		</div>
+	);
+}
--- 12-practice-project/src/components/Quiz/Quiz.jsx
+++ 12-practice-project/src/components/Quiz/Quiz.jsx
@@ -1,7 +1,12 @@
+import { QuestionOverview } from "../QuestionOverview/QuestionOverview";
 import "./Quiz.css";
 
 function Quiz() {
-	return <main className="quiz"></main>;
+	return (
+		<main className="quiz">
+			<QuestionOverview></QuestionOverview>
+		</main>
+	);
 }
 
 export default Quiz;
