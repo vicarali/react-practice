f06c98ece65189dcda164f679f1a490c53e64f8a
VictorC <vicarali@gmail.com>
2024-04-21 15:52:57 +0200

    Add progress bar to question overview

--- 12-practice-project/src/components/QuestionOverview/QuestionOverview.jsx
+++ 12-practice-project/src/components/QuestionOverview/QuestionOverview.jsx
@@ -3,7 +3,11 @@ import "./QuestionOverview.css";
 export function QuestionOverview() {
 	return (
 		<div className="question-overview">
-			<h2 className="question-overview__title">Test of a title</h2>
+			<progress
+				className="question-overview__progress-bar"
+				max="100"
+			></progress>
+			<label className="question-overview__title">Test of a title</label>
 		</div>
 	);
 }
