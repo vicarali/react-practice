3cd5b732124d06710a512dce65c24eb324c821fe
VictorC <vicarali@gmail.com>
2024-04-21 17:18:13 +0200

    Shuffle array of answers before rendering them

--- 12-practice-project/src/components/Answers/Answers.jsx
+++ 12-practice-project/src/components/Answers/Answers.jsx
@@ -1,15 +1,22 @@
 import "./Answers.css";
 
 export function Answers({ children }) {
-	return (
-		<ul className="answers">
-			{children.map((answer, index) => {
-				return (
-					<li className="answers__item" key={index}>
-						<button className="answers__button">{answer}</button>
-					</li>
-				);
-			})}
-		</ul>
-	);
+	let answerItems = children.map((answer, index) => {
+		return (
+			<li className="answers__item" key={index}>
+				<button className="answers__button">{answer}</button>
+			</li>
+		);
+	});
+
+	shuffleArray(answerItems);
+
+	return <ul className="answers">{answerItems}</ul>;
+}
+
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
 }
