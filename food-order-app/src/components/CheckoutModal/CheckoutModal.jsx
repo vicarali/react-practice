2688c46a79de32a472ab7db725a93a1c8469aabb
vca <vicarali@gmail.com>
2025-04-03 13:52:51 +0200

    feat: add form validation in the checkout form using form actions

--- food-order-app/src/components/CheckoutModal/CheckoutModal.jsx
+++ food-order-app/src/components/CheckoutModal/CheckoutModal.jsx
@@ -1,9 +1,13 @@
 import { CartContext } from "../../store/cart-context.jsx";
-import { use } from "react";
+import { use, useActionState } from "react";
 import styles from "./CheckoutModal.module.css";
 
 export default function CheckoutModal({ ref, setOrderState }) {
   const cartContext = use(CartContext);
+  const [checkoutActionState, checkoutFormAction] = useActionState(
+    checkoutAction,
+    { errors: null },
+  );
 
   return (
     <dialog className="modal" ref={ref}>
@@ -11,47 +15,70 @@ export default function CheckoutModal({ ref, setOrderState }) {
 
       <p>Total amount: {cartContext.cartTotal}€</p>
 
-      <form>
+      <form action={checkoutFormAction}>
         <div className={styles.control}>
-          <label htmlFor="full-name" className={styles.controlLabel}>
+          <label htmlFor="fullName" className={styles.controlLabel}>
             Full Name
           </label>
-          <input type="text" className={styles.controlInput} name="full-name" />
+          <input
+            type="text"
+            className={styles.controlInput}
+            name="fullName"
+            defaultValue={checkoutActionState.formFields?.fullName}
+          />
         </div>
         <div className={styles.control}>
-          <label htmlFor="email-address" className={styles.controlLabel}>
+          <label htmlFor="emailAddress" className={styles.controlLabel}>
             E-Mail Address
           </label>
           <input
             type="email"
             className={styles.controlInput}
-            name="email-address"
+            name="emailAddress"
+            defaultValue={checkoutActionState.formFields?.emailAddress}
           />
         </div>
         <div className={styles.control}>
           <label htmlFor="street" className={styles.controlLabel}>
             Street
           </label>
-          <input type="text" className={styles.controlInput} name="street" />
+          <input
+            type="text"
+            className={styles.controlInput}
+            name="street"
+            defaultValue={checkoutActionState.formFields?.street}
+          />
         </div>
         <div className={styles.controlRow}>
           <div className={styles.control}>
-            <label htmlFor="postal-code" className={styles.controlLabel}>
+            <label htmlFor="postalCode" className={styles.controlLabel}>
               Postal Code
             </label>
             <input
               type="text"
               className={styles.controlInput}
-              name="postal-code"
+              name="postalCode"
+              defaultValue={checkoutActionState.formFields?.postalCode}
             />
           </div>
           <div className={styles.control}>
             <label htmlFor="city" className={styles.controlLabel}>
               City
             </label>
-            <input type="text" className={styles.controlInput} name="city" />
+            <input
+              type="text"
+              className={styles.controlInput}
+              name="city"
+              defaultValue={checkoutActionState.formFields?.city}
+            />
           </div>
         </div>
+
+        {checkoutActionState.errors ? (
+          <p className={styles.controlError}>{checkoutActionState.errors}</p>
+        ) : (
+          ""
+        )}
 
         <div className="modal__actions">
           <button
@@ -68,4 +95,30 @@ export default function CheckoutModal({ ref, setOrderState }) {
       </form>
     </dialog>
   );
+}
+
+function checkoutAction(currentState, formData) {
+  const fullName = formData.get("fullName");
+  const emailAddress = formData.get("emailAddress");
+  const street = formData.get("street");
+  const postalCode = formData.get("postalCode");
+  const city = formData.get("city");
+  const formFields = {
+    fullName,
+    emailAddress,
+    street,
+    postalCode,
+    city,
+  };
+
+  if (
+    fullName === "" ||
+    emailAddress === "" ||
+    street === "" ||
+    postalCode === "" ||
+    city === ""
+  )
+    return { errors: "Please fill in all fields.", formFields };
+
+  return { errors: null, formFields };
 }
