3a62d9f081fd38df737e05efe530dcf08bf86168
vca <vicarali@gmail.com>
2025-04-25 14:02:26 +0200

    feat(memory-game): set up styling and initial items grid

--- reactpracticedotdev/memory-game/src/components/MemoryGame.jsx
+++ reactpracticedotdev/memory-game/src/components/MemoryGame.jsx
@@ -1,16 +1,21 @@
+import styles from "./MemoryGame.module.css";
+
 const MemoryGame = ({ images }) => {
   return (
     <div>
-      <h1>Memory Game</h1>
-      <p>Build your memory game! </p>
-      <p>Here are the sample images:</p>
-      {images.map((image) => (
-        <img
-          key={image}
-          src={image}
-          style={{ width: "300px", padding: "10px" }}
-        />
-      ))}
+      <h1 className={styles.title}>Memory Game</h1>
+      <ul className={styles.gameBoard}>
+        {images.map((image) => (
+          <li key={image} className={styles.pictureContainer}>
+            <img src={image} className={styles.picture} alt="Game picture" />
+          </li>
+        ))}
+        {images.map((image) => (
+          <li key={image} className={styles.pictureContainer}>
+            <img src={image} className={styles.picture} alt="Game picture" />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
