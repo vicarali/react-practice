11904b56b2ba4cb445ad9ce8da4518059b5286aa
vca <vicarali@gmail.com>
2025-04-26 17:03:33 +0200

    feat(memory-game): solution for exercise

--- reactpracticedotdev/memory-game/src/App.jsx
+++ reactpracticedotdev/memory-game/src/App.jsx
@@ -1,19 +1,35 @@
 import "./App.css";
 import MemoryGame from "./components/MemoryGame.jsx";
 
+const images = [
+  {
+    url: "https://images.unsplash.com/photo-1626808642875-0aa545482dfb",
+    id: 1,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1546842931-886c185b4c8c",
+    id: 2,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1520763185298-1b434c919102",
+    id: 3,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1442458017215-285b83f65851",
+    id: 4,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1496483648148-47c686dc86a8",
+    id: 5,
+  },
+  {
+    url: "https://images.unsplash.com/photo-1591181520189-abcb0735c65d",
+    id: 6,
+  },
+];
+
 function App() {
-  return (
-    <MemoryGame
-      images={[
-        "https://images.unsplash.com/photo-1626808642875-0aa545482dfb",
-        "https://images.unsplash.com/photo-1546842931-886c185b4c8c",
-        "https://images.unsplash.com/photo-1520763185298-1b434c919102",
-        "https://images.unsplash.com/photo-1442458017215-285b83f65851",
-        "https://images.unsplash.com/photo-1496483648148-47c686dc86a8",
-        "https://images.unsplash.com/photo-1591181520189-abcb0735c65d",
-      ]}
-    />
-  );
+  return <MemoryGame images={images} />;
 }
 
 export default App;
--- reactpracticedotdev/memory-game/src/components/MemoryGame.jsx
+++ reactpracticedotdev/memory-game/src/components/MemoryGame.jsx
@@ -1,27 +1,67 @@
 import styles from "./MemoryGame.module.css";
 import lodash from "lodash";
+import { useEffect, useRef, useState } from "react";
 
 const MemoryGame = ({ images }) => {
-  const generatedBoardImages = generateBoardImages(images);
+  const [boardImages, setBoardImages] = useState([]);
+  const [firstImage, setFirstImage] = useState(null);
+  const [secondImage, setSecondImage] = useState(null);
+  const currentImageNode = useRef(null);
+
+  useEffect(() => {
+    setBoardImages(generateBoardImages(images));
+  }, [setBoardImages, images]);
+
+  function generateBoardImages(images) {
+    const copiedImages = images.map((image) => {
+      return { ...image, id: image.id + images.length };
+    });
+
+    return lodash.shuffle([...images, ...copiedImages]);
+  }
+
+  function handleImageClick(event, image) {
+    event.target.classList.add("selected");
+
+    if (!firstImage && !secondImage && currentImageNode.current) {
+      setFirstImage(image);
+      currentImageNode.current.classList.remove("selected");
+    } else if (!firstImage && !secondImage) {
+      setFirstImage(image);
+    } else if (firstImage && secondImage) {
+      setFirstImage(image);
+      setSecondImage(null);
+    } else if (firstImage.url !== image.url) {
+      currentImageNode.current.classList.remove("selected");
+      setFirstImage(image);
+      setSecondImage(null);
+    } else {
+      setSecondImage(image);
+    }
+
+    currentImageNode.current = event.target;
+  }
 
   return (
     <div>
       <h1 className={styles.title}>Memory Game</h1>
       <ul className={styles.gameBoard}>
-        {generatedBoardImages.map((image) => (
-          <li key={image} className={styles.pictureContainer}>
-            <img src={image} className={styles.picture} alt="Game picture" />
+        {boardImages.map((image) => (
+          <li
+            key={image.id}
+            className={styles.pictureContainer}
+            onClick={(event) => handleImageClick(event, image)}
+          >
+            <img
+              src={image.url}
+              className={styles.picture}
+              alt="Game picture"
+            />
           </li>
         ))}
       </ul>
     </div>
   );
 };
-
-function generateBoardImages(images) {
-  const copiedImages = [...images];
-
-  return lodash.shuffle([...images, ...copiedImages]);
-}
 
 export default MemoryGame;
